af0040be17439585d89e30ba16a30d6d848e4a94
Carlos Romero <carlosonasisromero@gmail.com>
2022-05-08 13:33:35 -0400

    Barajas de cartas creada

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,22 @@
-console.log('Hello');
+let deck = [];
+const types = ['C', 'D', 'H', 'S'];
+const specials = ['A','J','Q','K'];
+
+const createDeck = ()=>{
+
+    for(let i = 2; i <= 10; i++){
+        for (const type of types){
+            deck.push(`${i + type}`);
+        }
+    }
+
+    for (const special of specials){
+        for (const type of types){
+            deck.push(`${special + type}`);
+        }
+    }
+    
+    deck = _.shuffle(deck);
+    return deck;
+}
+createDeck();
