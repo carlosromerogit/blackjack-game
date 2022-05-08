dea942c2a921231b36bd67f9ccae4c5dd1c90e54
Carlos Romero <carlosonasisromero@gmail.com>
2022-05-08 16:54:25 -0400

    Cartas de forma dinámica

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,16 @@
 let deck = [];
+let playerTotalScore = 0;
+let computerScore = 0;
 const types = ['C', 'D', 'H', 'S'];
 const specials = ['A','J','Q','K'];
+
+//HTML references
+const btnNewGame = document.querySelector('#btnNewGame');
+const btnAskCard = document.querySelector('#btnAskCard');
+const btnStop = document.querySelector('#btnAskCard');
+const playerScore = document.querySelector('#playerScore');
+const cardsContainer = document.querySelectorAll('.cards-container');
+const result = document.querySelector('.result');
 
 const createDeck = ()=>{
 
@@ -35,4 +45,28 @@ const cardValue = (card)=>{
     const value = card.substring(0, card.length - 1);
     return (!isNaN(value)) ? Number(value) : ( value === 'A') ? 11 : 10;
 }
-console.log(cardValue(askForCard()));
+
+//DOM events
+btnAskCard.addEventListener('click', ()=>{
+
+    const card = askForCard();
+    const value = cardValue(askForCard());
+    playerScore.innerText = playerTotalScore += value;
+
+    const imgCard = document.createElement('img');
+    imgCard.src = `/assets/cards/${card}.png`;
+    imgCard.classList.add('card');
+    cardsContainer[0].append(imgCard);
+
+    if(Number(playerScore.innerHTML) > 21){
+        result.style.color = '#CB4335';
+        result.innerText = 'Perdiste';
+        btnAskCard.disabled = true;
+    }else if(Number(playerScore.innerHTML) === 21){
+        result.style.color = '#2ECC71';
+        result.innerText = 'Genial, obtuviste 21!';
+        btnAskCard.disabled = true;
+    }
+    
+})
+
