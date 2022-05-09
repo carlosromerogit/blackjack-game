511f42b44ac83ff7d6dee422c02dab11b243469c
Carlos Romero <carlosonasisromero@gmail.com>
2022-05-09 10:16:32 -0400

    Nuevo juego y mensajes

--- assets/js/game.js
+++ assets/js/game.js
@@ -13,41 +13,34 @@ const cardsContainer = document.querySelectorAll('.cards-container');
 const result = document.querySelector('.result');
 
 const createDeck = ()=>{
-
     for(let i = 2; i <= 10; i++){
         for (const type of types){
             deck.push(`${i + type}`);
         }
     }
-
     for (const special of specials){
         for (const type of types){
             deck.push(`${special + type}`);
         }
     }
-
     deck = _.shuffle(deck);
     return deck;
 }
 createDeck();
 
 const askForCard = ()=>{
-
     if(deck.length === 0){
         throw 'No hay más cartas en el deck'
     }
-
     const card = deck.pop();
     return card;
 }
-
 const cardValue = (card)=>{
     const value = card.substring(0, card.length - 1);
     return (!isNaN(value)) ? Number(value) : ( value === 'A') ? 11 : 10;
 }
 //Computer turn
 const computerLogic = (playerScore)=>{
-
     do {
         const card = askForCard();
         const value = cardValue(askForCard());
@@ -57,18 +50,27 @@ const computerLogic = (playerScore)=>{
         imgCard.src = `/assets/cards/${card}.png`;
         imgCard.classList.add('card');
         cardsContainer[1].append(imgCard);
-        
     } while ((computerTotalScore < playerScore) && (computerTotalScore <= 21));
 
 }
-
 //DOM events
+btnNewGame.addEventListener('click', ()=>{
+    deck = [];
+    deck = createDeck();
+    playerTotalScore = 0;
+    computerTotalScore = 0;
+    htmlScore[0].innerHTML = 0;
+    htmlScore[1].innerHTML = 0
+    btnAskCard.disabled = false;
+    btnStop.disabled = false;
+    result.innerHTML = '';
+    cardsContainer[0].innerHTML = '';
+    cardsContainer[1].innerHTML = '';
+});
 btnAskCard.addEventListener('click', ()=>{
-
     const card = askForCard();
     const value = cardValue(askForCard());
     htmlScore[0].innerText = playerTotalScore += value;
-
     const imgCard = document.createElement('img');
     imgCard.src = `/assets/cards/${card}.png`;
     imgCard.classList.add('card');
@@ -79,28 +81,44 @@ btnAskCard.addEventListener('click', ()=>{
         result.innerText = 'Perdiste';
         btnAskCard.disabled = true;
         btnStop.disabled = true;
-
         computerLogic(value);
-       
     }else if(Number(htmlScore[0].innerHTML) === 21){
         result.style.color = '#2ECC71';
         result.innerText = 'Genial, obtuviste 21!';
         btnAskCard.disabled = true;
         btnStop.disabled = true;
-
     }
-})
-
+});
 btnStop.addEventListener('click', ()=>{
-    
     if(playerTotalScore === 21){
+        btnAskCard.disabled = true;
         btnStop.disabled = true;
         return;
     }
-
-    computerLogic(playerTotalScore);
-
-    console.log('clickss')
-
+     computerLogic(playerTotalScore);
+    if(computerTotalScore === 21){
+        result.style.color = '#CB4335';
+        result.innerText = 'Perdiste!';
+        btnAskCard.disabled = true;
+        btnStop.disabled = true;
+    } 
+    if((computerTotalScore > playerTotalScore) && computerTotalScore < 21){
+        result.style.color = '#CB4335';
+        result.innerText = 'Perdiste!';
+        btnAskCard.disabled = true;
+        btnStop.disabled = true;
+    }
+    if((computerTotalScore > playerTotalScore) && computerTotalScore > 21){
+        result.style.color = '#2ECC71';
+        result.innerText = 'Ganaste!';
+        btnAskCard.disabled = true;
+        btnStop.disabled = true;
+    }
+    if(computerTotalScore === playerTotalScore){
+        result.style.color = '#F4D03F';
+        result.innerText = 'Empate!';
+        btnAskCard.disabled = true;
+        btnStop.disabled = true;
+    }
 });
 
